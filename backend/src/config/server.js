787f1111ccdf396194f20e09fdc7641bd64b1b30
leandrolimasi <leandrolimadasilva@gmail.com>
2019-06-11 08:34:53 -0300

    add cors config

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,8 +4,11 @@ const express = require('express');
 const server = express();
 const port = 3003;
 
+const allowCors = require('./cors');
+
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(allowCors);
 
 server.listen(port, function () {
     console.log(`sever running on port ${port}.`);

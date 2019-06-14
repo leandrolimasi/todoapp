cb58b9e91e8adb5298c1f25c99ac7fe48771bc0a
leandrolimasi <leandrolimadasilva@gmail.com>
2019-06-14 08:30:36 -0300

    fix to start web dev server

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,8 +1,8 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css';
-import 'modules/font-awesome/css/font-awesome.min/css';
+import 'modules/font-awesome/css/font-awesome.min.css';
 import React from 'react';
 
-export default props =>(
+export default props => (
     <div className="container">
         <h1>Test</h1>
     </div>
